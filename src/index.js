135bf4fb1c59423827b3140e324f230e07202815
lzy <326656669@qq.com>
2017-10-20 15:39:28 +0800

    添加TodoInput TodoItem 文件 修改index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import Welcome from './Welcome'
+import App from './App'
 
 ReactDOM.render(
-  <Welcome name="frank"/>,
+  <App />,
   document.getElementById('root')
 )
