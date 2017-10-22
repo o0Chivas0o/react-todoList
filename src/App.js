2e050e00bc4d9cc9890cfbc1b225ec4fe442cf26
lzy <326656669@qq.com>
2017-10-22 17:57:16 +0800

    测试leancloud

--- src/App.js
+++ src/App.js
@@ -7,11 +7,19 @@ import TodoItem from './TodoItem'
 import * as localStore from './localStore'
 
 import AV from 'leancloud-storage'
-var APP_ID = WlkFA7xp5f8YICU2BCzU15VJ-gzGzoHsz
-var APP_KEY = TUEDVMVLmldCMoL0I8NbP619
+var APP_ID = 'rUcmyykR2Tsi5VOPjfnkM7Yj-gzGzoHsz'
+var APP_KEY = 'Oy9HGah8eknBB6yTb9LOKjrt'
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY
+})
+
+var TestObject = AV.Object.extend('TestObject')
+var testObject = new TestObject()
+testObject.save({
+  words: 'Hello World!'
+}).then(function(object) {
+  alert('LeanCloud Rocks!')
 })
 
 class App extends Component {
