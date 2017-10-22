278386a58682d6a77758790dce075fef5a249cff
lzy <326656669@qq.com>
2017-10-22 21:58:51 +0800

    用户点击发送时，请求 LeanCloud 的 API

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -135,7 +135,6 @@ export default class UserDialog extends Component{
           </div>
           <div className="row actions">
             <button type="submit">发送重置邮件</button>
-            <a href="#" onClick={this.returnToSignIn.bind(this)}>返回登录</a>
           </div>
         </form>
       </div>
@@ -151,11 +150,6 @@ export default class UserDialog extends Component{
   showForgotPassword(){
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.selectedTab = 'forgotPassword'
-    this.setState(stateCopy)
-  }
-  returnToSignIn(){
-    let stateCopy = JSON.parse(JSON.stringify(this.state))
-    stateCopy.selectedTab = 'signInOrSignUp'
     this.setState(stateCopy)
   }
   resetPassword(e){
